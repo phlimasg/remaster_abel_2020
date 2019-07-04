dec79963bea39556b03b36ea754ae191ca611ddd
phlimasg <raphaelpc_@hotmail.com>
2019-07-04 17:20:43 -0300

    inserindo css 3

--- abel_js_2020.js
+++ abel_js_2020.js
@@ -1,3 +1,3 @@
 //TI Abel
 //Adicionando o css personalizado
-document.getElementById("header").innerHTML = '<link rel="stylesheet" href="http://dev2-new.abel.org.br/testes/remaster_abel_2020/css_abel_2020.css">';
+document.getElementById("header").innerHTML += '<link rel="stylesheet" href="http://dev2-new.abel.org.br/testes/remaster_abel_2020/css_abel_2020.css">';
